ed0f385d37c1defb9b9539732583146e5eb33a56
Sheetal6378 <98173383+Sheetal6378@users.noreply.github.com>
2023-04-16 08:10:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
-import Java.util.*;
-Class solution {
+
+class Solution {
   public static void main (String args[]){
-   Scanner sc = new Scanner(System.in);
-   System.out.print("Hello java");
+   System.out.println("Hello java");
+}
 }
