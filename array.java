d6eb55f85e96aa92268e6c7e3f77dcdbcba99c89
Sheetal6378 <98173383+Sheetal6378@users.noreply.github.com>
2023-04-17 20:12:18 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Arrays{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    int size =sc.nextInt();
+    int numbers[]=new int[size];
+    for(int i=0; i<size;i++){
+      number[i]=sc.nextInt();
+      for(int i=0; i<arr.length;i++){
+        System.out.print(numbers[i]+"");
+      }
+    }
+  }
+ 
