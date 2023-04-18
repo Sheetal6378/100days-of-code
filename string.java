e6c97d25578549bc0761539cb165fdf4ef14831f
Sheetal6378 <98173383+Sheetal6378@users.noreply.github.com>
2023-04-18 20:00:24 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,10 @@
+String firstName = "Tony";
+       String secondName = "Stark";
+
+
+       String fullName = firstName + " " + secondName;
+
+
+       for(int i=0; i<fullName.length(); i++) {
+           System.out.println(fullName.charAt(i));
+       }
