ea8366ab095d5b9fb395bb304b19e9ad39c68910
Sheetal6378 <98173383+Sheetal6378@users.noreply.github.com>
2023-04-28 18:46:29 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,2 +1,27 @@
-class polymorphism{
-  public static void main(String args[])
+class shapes {
+  public void render() {
+    System.out.println("square...");
+  }
+}
+
+class circle extends square{
+  public void render() {
+    System.out.println("circle...");
+  }
+}
+
+class triangle extends shapes {
+  public void render() {
+    System.out.println("triangle...");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Square s1 = new Square();
+    s1.render();
+
+    Circle c1 = new Circle();
+    c1.render();
+  }
+}
